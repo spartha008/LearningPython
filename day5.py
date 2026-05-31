b59de49d76f6e398ebc67b5af98bdb73062260e2
spartha008 <mohammedamees82@gmail.com>
2026-05-31 21:11:08 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,14 @@
+"""
+Hey Harry, Please dont remove this line
+Author : Harry
+Course : 100DaysOfCode
+"""
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy/girl")
+
+# # print("Hello World")
+# # This is a single line comment 
+# print("This is a print statement.")
+# print("Hello World !!!") # Printing Hello World
+
+print("Hey", 6, 7, sep="-", end="009\n")
+print("Harry")
---
+++ day5prac.py
@@ -0,0 +1,4 @@
+# This is me printing a poem which has the use of double inverted commas
+# for the practice of the things learnt on day 5.
+
+print("The night whispered a secret low, \n\"The stars shine brightest when they go.\"\nThe moon replied with silver grace,\n\"True light belongs to time and space.\"")
