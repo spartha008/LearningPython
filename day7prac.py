c74a436619d6368d24d00afcd1547446064cb163
spartha008 <mohammedamees82@gmail.com>
2026-05-31 22:00:25 +0530

    Create day7prac.py

---
+++ day7prac.py
@@ -0,0 +1,9 @@
+# Creating a Calculator Problem
+# Disqualified as input function wasnt taught
+a = int(input("Enter the number:"))
+b = int(input("Enter another number:"))
+sum = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print(sum, sub, mul, div)
