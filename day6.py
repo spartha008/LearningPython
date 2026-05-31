9ff262831d26c7867ada363124987e7fdc1030a1
spartha008 <mohammedamees82@gmail.com>
2026-05-31 21:25:10 +0530

    Enhance day6.py with comments on data types
    Added comments explaining data types and collections in Python.

--- day6.py
+++ day6.py
@@ -6,15 +6,17 @@ print(a)
 print(b)
 a1 = 9
 print(a + a1)
-print("The type of a is ", type(a))
-print("The type of b is ", type(b))
-print("The type of c is ", type(c))
+print("The type of a is ", type(a)) #Integer data type consists of i) integer (3, -8, 0) ii) float (7.348, -9.0, 0.00000001) iii) complex (6+2i)
+print("The type of b is ", type(b)) #Boolean data type consists of values True or False
+print("The type of c is ", type(c)) #String data type consists of alphabets
 
-list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]] #List is an ordered collection of data types OR List is a list of items which can be of different data types
 print(list1)
 
-tuple1 = (("parrot", "sparrow"), ("lion", "Tiger"))
+tuple1 = (("parrot", "sparrow"), ("lion", "Tiger")) #Tuple is the same thing as a list except they are immutable (cant be changed)
 print(tuple1)
 
-dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+dict1 = {"name":"Sakshi", "age":20, "canVote":True} #Dictionary is the collection of key-value pairs
 print(dict1)
+
+#Everything in python is an object.
