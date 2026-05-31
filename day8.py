0b89321bc4fec1f1abcb2e16d7218a495dfcbb98
spartha008 <mohammedamees82@gmail.com>
2026-05-31 22:01:06 +0530

    Create day8.py

---
+++ day8.py
@@ -0,0 +1,10 @@
+# Solution of Calculator Problem
+# alt + shift + dwn = replicate lines in vs code
+
+a = 50
+b = 3
+
+print("The value of", a, "+", b, "is: ", a+b)
+print("The value of", a, "-", b, "is: ", a-b)
+print("The value of", a, "*", b, "is: ", a*b)
+print("The value of", a, "/", b, "is: ", a/b)
