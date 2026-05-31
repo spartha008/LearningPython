e15ccf89de769de6e7e6016d05ee7692198cac73
spartha008 <mohammedamees82@gmail.com>
2026-05-31 21:58:49 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,8 @@
+# Arithmetic Operators
+print(5+6) # Addition
+print(15-6) # Subtraction
+print(15*6) # Multiplication
+print(15/6) # Division
+print(15//6) # Floor Division (Ignores the digits after decimal)
+print(5%3) # Modulus Operator gives the remainder after division
+print(5**3) # Exponential Operator (5X5X5)
