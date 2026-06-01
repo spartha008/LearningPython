824ebbbf7cc12070e57a0a010ae1600a853c0efe
spartha008 <mohammedamees82@gmail.com>
2026-06-01 15:22:12 +0000

    done with day 14

---
+++ day14-03Nested.py
@@ -0,0 +1,30 @@
+'''
+num = 18
+if (num < 0):
+    print("Number is negative.")
+elif (num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num > 10 and num <=20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
+
+'''
+
+
+num = int(input("Please enter a number: "))
+if (num < 0):
+    print("Number is negative")
+elif(num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+   
+else:
+    print("Number is zero")
