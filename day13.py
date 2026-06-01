6260a5ea16bc28c46b03a9365e65d5a61113d9b7
spartha008 <mohammedamees82@gmail.com>
2026-06-01 13:02:19 +0000

    done with day 13

--- day13.py
+++ day13.py
@@ -24,5 +24,23 @@ print(str2.find("is")) # Searches for the first occurance of the given charecter
 print(str2.index("is")) # Searches for the first occurance of the given value and returns the index where it is present
     # Raises exception if the given value is absent from the string
 
-str3 = "WelcomeToTheConsole"
+str3 = "WelcomeToTheConsole0"
 print(str3.isalnum()) # Checks if the given string is alphanumeric (A-Z, a-z, 0-9) in boolean data type
+print(str3.isalpha()) # Checks if the given string is aplha
+print(str3.islower()) # Checks if the given string is in lower case 
+
+str4 = "We wish you a Merry Christmas\n"
+print(str4.isprintable()) # checks if the given string is printable
+
+str5 = "        " # Using Spacebar
+str6 = "        " # Using tab
+print(str5.isspace()) # True if the sting contains white spaces
+print(str6.isspace())  
+
+str7 = "World Health Organization"
+print(str7.istitle()) # Checks if every word of the string is a Capital
+
+str8 = "Python is a Interpreted Language"
+print(str8.startswith("Python")) # Checks if the given string starts with the given charecters
+print(str8.swapcase()) # Swaps the cases of the words in the string
+print(str8.title()) # Capitalizes each word within the string 
