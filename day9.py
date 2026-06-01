bf39d58bcd79bae38bd18b26d8ff394a19889b87
spartha008 <mohammedamees82@gmail.com>
2026-06-01 07:27:22 +0000

    done with half of the vid

---
+++ day9.py
@@ -0,0 +1,10 @@
+# Typecasting in Python 
+# The conversion of one data type into the other data type is known as type casting in python
+# Python supports a wide veriety of functions or methods like: 
+# int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict(), etc., for the type casting in python
+
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
