559cec0420e0940d662c9664b753fc0d29178bf2
spartha008 <mohammedamees82@gmail.com>
2026-06-01 14:55:09 +0000

    1st part of day 14 done

---
+++ day14.py
@@ -0,0 +1,16 @@
+# If else conditional statements
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+# Conditional Operators
+# >, <, >=, <=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>18):
+    print("You can drive")
+    print("Yes")
+else:
+    print("You cannot drive")
+    print("Yay!")
+
