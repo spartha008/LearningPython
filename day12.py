2bbaa0a24ee323e3e92c4c198d6409c452e30ae7
spartha008 <mohammedamees82@gmail.com>
2026-06-01 11:15:28 +0000

    done with day 12

---
+++ day12.py
@@ -0,0 +1,19 @@
+# Strings Slicing and Operations on Strings in Python
+# names = "Harry, Shubam"
+# print(names[0:5])
+# print(len(names))
+
+# fruit = "Mango"
+# len1 = len(fruit)
+# print("Mango is a", len1, "letter word.")
+
+fruit = "Mango"
+mangolen = len(fruit)
+print(mangolen)
+print(fruit[0:4])
+print(fruit[:4])
+print(fruit[1:4])
+print(fruit[0:-3])
+print(fruit[0:len(fruit)-3])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
