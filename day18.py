831e5131fd29cf83e082e5b98c98f5b385977de2
spartha008 <mohammedamees82@gmail.com>
2026-06-03 14:11:11 +0000

    done with day

---
+++ day18.py
@@ -0,0 +1,24 @@
+# While loop - execute statements while the condition is true; as soon as the condition becomes false, the interpreter comes out of the while loop
+
+# For loop-
+# for i in range(3):
+#     print(i)
+
+# While loop-
+i = int(input("Enter the number: "))
+while(i<=38):
+    i = int(input("Enter the number: "))
+    print(i)
+
+print("Done with the loop")
+
+# Decrementing While loop-
+count = 5
+while (count > 0):
+    print(count)
+    count = count - 1
+# Else with while loop-
+else:
+    print("I am inside else")
+
+# Do while loop in c++ or java is executed at least once irrespective if the condition is met or not
